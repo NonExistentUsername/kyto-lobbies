80fe85cf65df9f2c75362252b4c8da8467ceb013
Dmytro Maior <mayor4090Gmail.com>
2023-09-09 20:01:07 +0300

    add get_message_bus deps

--- engine/entrypoints/fastapi_app/deps.py
+++ engine/entrypoints/fastapi_app/deps.py
@@ -1,8 +1,16 @@
+from adapters.repository import RamRepository
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+from service_player.messagebus import MessageBus
+from service_player.unit_of_work import RamUnitOfWork
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+message_bus = MessageBus(RamUnitOfWork(RamRepository()), [], {})
 
 
 async def get_current_user(uuid: str = Depends(oauth2_scheme)) -> str:
     return uuid
+
+
+async def get_message_bus() -> MessageBus:
+    return message_bus
