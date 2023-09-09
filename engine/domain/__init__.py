9a92fb39e316d53a7aa615b5b22ab7357066e8e5
Dmytro Maior <mayor4090Gmail.com>
2023-09-09 18:05:10 +0300

    add commands to domain

--- engine/domain/__init__.py
+++ engine/domain/__init__.py
@@ -2,5 +2,6 @@ from .events import Event
 from .game import Game, State
 from .player import Player
 from .room import Room
+from .commands import Command
 
-__all__ = ["Event", "Game", "State", "Player", "Room"]
+__all__ = ["Event", "Game", "State", "Player", "Room", "Command"]
