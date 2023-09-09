b24480a6aec252ed9543a20b1c070a016c658ecb
Dmytro Maior <mayor4090Gmail.com>
2023-09-09 21:07:30 +0300

    add docs and refactoring

--- engine/entrypoints/fastapi_app/v1/routers.py
+++ engine/entrypoints/fastapi_app/v1/routers.py
@@ -1,6 +1,10 @@
 from entrypoints.fastapi_app.v1.players import router as players_router
 from fastapi import APIRouter
 
+"""
+Here we will include all routers
+"""
+
 router = APIRouter(
     prefix="/v1",
     tags=["api_v1"],
