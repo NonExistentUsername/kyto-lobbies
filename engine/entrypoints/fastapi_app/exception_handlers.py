88a787de1d8752386dc42fa529b1aa316152b60b
Dmytro Maior <mayor4090Gmail.com>
2023-09-09 21:04:15 +0300

    add rewrite_404_exception handler

---
+++ engine/entrypoints/fastapi_app/exception_handlers.py
@@ -0,0 +1,15 @@
+from entrypoints.fastapi_app.responses import Response
+from fastapi import Request
+from fastapi.exceptions import HTTPException
+from fastapi.responses import JSONResponse
+
+
+def rewrite_404_exception(request: Request, exc: HTTPException) -> JSONResponse:
+    return JSONResponse(
+        status_code=404,
+        content=Response(
+            message="Not found",
+            status_code=404,
+            success=False,
+        ),
+    )
