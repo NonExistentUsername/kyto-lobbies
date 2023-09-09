4685aa3cf4dc4ea47195e6f3ad632a727cd6e229
Dmytro Maior <mayor4090Gmail.com>
2023-09-09 19:40:22 +0300

    add create player endpoint

---
+++ engine/entrypoints/fastapi_entrypoint/v1/players.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+
+from engine.entrypoints.fastapi_entrypoint.deps import get_current_user
+
+players_router = APIRouter(
+    prefix="/players",
+    tags=["players"],
+    dependencies=[Depends(get_current_user)],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@players_router.post("/")
+async def create_player():
+    return {"message": "Hello World"}
