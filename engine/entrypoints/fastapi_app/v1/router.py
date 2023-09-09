9202be9e00ef4499d94c139f79eca0b1a495ed27
Dmytro Maior <mayor4090Gmail.com>
2023-09-09 21:02:25 +0300

    update router

--- engine/entrypoints/fastapi_app/v1/router.py
+++ engine/entrypoints/fastapi_app/v1/router.py
@@ -4,7 +4,6 @@ from fastapi import APIRouter
 router = APIRouter(
     prefix="/v1",
     tags=["api_v1"],
-    responses={404: {"description": "Not found"}},
 )
 
 router.include_router(players_router)
