71fc29665682ce4894035ebad8a50640b85e6dd2
Dmytro Maior <mayor4090Gmail.com>
2023-09-10 20:40:40 +0300

    add base model in domain

---
+++ engine/domain/base.py
@@ -0,0 +1,13 @@
+class BaseModel:
+    def __init__(self, id: str):
+        self.__id = id
+
+    @property
+    def id(self) -> str:
+        """
+        Getter for id
+
+        Returns:
+            str: id
+        """
+        return self.__id
