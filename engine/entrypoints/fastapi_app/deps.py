db4a751d562846791de5d9e7533e8acd4b389fb7
Dmytro Maior <mayor4090Gmail.com>
2023-09-10 21:06:12 +0300

    update message bus creation way

--- engine/entrypoints/fastapi_app/deps.py
+++ engine/entrypoints/fastapi_app/deps.py
@@ -1,11 +1,10 @@
-from adapters.repository import RamRepository
+from factories import create_message_bus, create_uow
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from service_player.messagebus import MessageBus
-from service_player.unit_of_work import RamUnitOfWork
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-message_bus = MessageBus(RamUnitOfWork(RamRepository()), [], {})
+message_bus = create_message_bus(create_uow("ram"))
 
 
 async def get_current_user(uuid: str = Depends(oauth2_scheme)) -> str:
