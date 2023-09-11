44581e7047f67268cb5916e1469dd59a49d60451
Dmytro Maior <mayor4090Gmail.com>
2023-09-11 15:04:00 +0300

    update room model

--- engine/domain/rooms.py
+++ engine/domain/rooms.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from domain.base import BaseModel
 
@@ -7,9 +7,22 @@ if TYPE_CHECKING:
 
 
 class Room(BaseModel):
-    def __init__(self, id: str, players: list[players.Player]):
+    def __init__(
+        self, id: str, creator_id: str, players: Optional[list[players.Player]] = None
+    ):
         super().__init__(id)
-        self.__players = players
+        self.__creator_id = creator_id
+        self.__players = players or []
+
+    @property
+    def creator_id(self) -> str:
+        """
+        Getter for creator_id
+
+        Returns:
+            str: creator_id
+        """
+        return self.__creator_id
 
     @property
     def players(self) -> list[players.Player]:
