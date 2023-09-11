028c2b3d5cdcbc77e69bc960efd07a97b786d19c
Dmytro Maior <mayor4090Gmail.com>
2023-09-11 17:09:14 +0300

    add EventResult

--- engine/domain/events.py
+++ engine/domain/events.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Any
 from uuid import uuid4
 
 from domain import players, rooms
@@ -11,6 +12,26 @@ class Event:
     @property
     def id(self) -> str:
         return self.__id
+
+
+class EventResult:
+    def __init__(self, event: Event, success: bool, result: Any = None):
+        self.__id: str = str(uuid4())  # unique id of event response
+        self.__event: Event = event  # event that was executed
+        self.__success: bool = success  # whether event was executed successfully
+        self.__result: Any = result  # result of event execution
+
+    @property
+    def id(self) -> str:
+        return self.__id
+
+    @property
+    def event(self) -> Event:
+        return self.__event
+
+    @property
+    def success(self) -> bool:
+        return self.__success
 
 
 @dataclass
