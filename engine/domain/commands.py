fcf51f1a8613eb38def4dc561d6a0e718918168d
Dmytro Maior <mayor4090Gmail.com>
2023-09-11 17:08:42 +0300

    add CommandResult

--- engine/domain/commands.py
+++ engine/domain/commands.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Any
 from uuid import uuid4
 
 
@@ -9,6 +10,30 @@ class Command:
     @property
     def id(self) -> str:
         return self.__id
+
+
+class CommandResult:
+    def __init__(self, command: Command, success: bool, result: Any = None):
+        self.__id: str = str(uuid4())  # unique id of command response
+        self.__command: Command = command  # command that was executed
+        self.__success: bool = success  # whether command was executed successfully
+        self.__result: Any = result  # result of command execution
+
+    @property
+    def id(self) -> str:
+        return self.__id
+
+    @property
+    def command(self) -> Command:
+        return self.__command
+
+    @property
+    def success(self) -> bool:
+        return self.__success
+
+    @property
+    def result(self) -> Any:
+        return self.__result
 
 
 @dataclass
