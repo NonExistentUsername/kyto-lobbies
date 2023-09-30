3453714f24544ca3e34af1e899ff081345b5b18f
Dmytro Maior <mayor4090Gmail.com>
2023-09-30 17:52:35 +0300

    remove useless deps

--- engine/entrypoints/fastapi_app/deps.py
+++ engine/entrypoints/fastapi_app/deps.py
@@ -1,14 +1,7 @@
 from factories import create_message_bus, create_uow
-from fastapi import Depends
-from fastapi.security import OAuth2PasswordBearer
 from service_player.messagebus import MessageBus
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 message_bus = create_message_bus(create_uow("ram"))
-
-
-async def get_current_user(uuid: str = Depends(oauth2_scheme)) -> str:
-    return uuid
 
 
 async def get_message_bus() -> MessageBus:
